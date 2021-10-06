3e1736c22ff6aeef21953aaae6b8730a03938210
Andres2D <andres2d1997.1206@gmail.com>
2021-10-05 20:54:40 -0500

    Convert template page on handlebars components

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express();
 const port = 8080;
 
+// Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Serve static content
 app.use(express.static('public'));
@@ -16,11 +20,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html');
+    res.render('generic', {
+        name: 'Andres',
+        title: 'Node course'
+    });
 });
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements', {
+        name: 'Andres',
+        title: 'Node course'
+    });
 });
 
 app.get('*', (req, res) => {
